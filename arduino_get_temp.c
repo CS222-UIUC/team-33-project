61cf295685baaf525b07fb9e96fd0695d75bcaa0
Iamenescolaktoo <hcola2@illinois.edu>
2025-03-28 16:41:12 -0500

    Update arduino_get_temp.c

--- arduino_get_temp.c
+++ arduino_get_temp.c
@@ -1,28 +1,31 @@
 #include <Arduino.h>
 
-#define TEMP_SENSOR_PIN A0  // Define the analog pin for the temperature sensor
+#did a mini project to underestand how arduino's work
+const int ledPin = LED_BUILTIN;  // the number of the LED pin
+
+int ledState = LOW;  
+
+unsigned long previousMillis = 0;  
+
+const long interval = 1000;  // interval at which to blink (milliseconds)
 
 void setup() {
-    // Initialize the Serial communication at 9600 baud rate
-    Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-    // Read the raw ADC value from the temperature sensor
-    int sensorValue = analogRead(TEMP_SENSOR_PIN);
-    
-    // Convert the ADC value to a voltage (assuming a 5V reference)
-    float voltage = sensorValue * (5.0 / 1023.0);
-    
-    // For an LM35 sensor, 10mV corresponds to 1°C.
-    // Therefore, temperature in Celsius is voltage in volts multiplied by 100.
-    float temperatureC = voltage * 100.0;
-    
-    // Print the temperature to the Serial Monitor
-    Serial.print("Temperature: ");
-    Serial.print(temperatureC);
-    Serial.println(" C");
-    
-    // Wait for 1 second before taking another reading
-    delay(1000);
+
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+
+    if (ledState == LOW) {
+      ledState = HIGH;
+    } else {
+      ledState = LOW;
+    }
+
+    digitalWrite(ledPin, ledState);
+  }
 }
