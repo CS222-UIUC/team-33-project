a459dcff3837bdce614cbf8f8ee8aee1b392b14d
Furkan Akin <furkakin11@gmail.com>
2025-03-28 21:13:04 -0500

    Implemented API Integration

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from welcomePage.views import home
+from welcomePage.views import home, callAPI
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
+    path('weather/', callAPI ),
 ]
--- backend/welcomePage/views.py
+++ backend/welcomePage/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
+from dotenv import load_dotenv
+import os
 # Create your views here.
+
+
 
 def home(request):
     return HttpResponse('Welcome to TempWise!')
+
+def callAPI(request):
+    load_dotenv(os.path.join(os.path.dirname(__file__), "keys.env"))
+    key = os.getenv("API_KEY")
+    city = "Chicago"
+    response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={key}&units=metric")
+
+    weather_data = response.json()
+    
+    if response.status_code == 200:
+            context = {
+                "city": weather_data["name"],
+                "temperature": weather_data["main"]["temp"],
+                "weather": weather_data["weather"][0]["description"],
+                "icon": weather_data["weather"][0]["icon"],
+            }
+    return render(request, "weather.html", context)
+
+
